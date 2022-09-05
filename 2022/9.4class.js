769d6f7292eed18021e06c69f1ee69994b7ab9eb
liujia <liujia12101@163.com>
2022-09-05 19:29:51 +0800

    面向对象

---
+++ 2022/9.4class.js
@@ -0,0 +1,28 @@
+// 最标准的面向对象实现模式  基于原型继承的构造函数
+// 创建一个圆构造函数
+// function Circle (center, radius){
+//   this.center = center
+//   this.radius = radius
+// }
+// // 计算圆的周长
+// Circle.prototype.getCircumReference = function(){
+//   return 2*Math.PI*this.radius
+// }
+
+// 使用class
+class Circle {
+  // 构造器
+  constructor(center, radius) {
+    this.center = center
+    this.radius = radius
+  }
+  // 方法
+  getCircumReference() {
+    return 2 * Math.PI * this.radius
+  }
+}
+
+// 创建一个圆的实例
+const c1 = new Circle(1, 5)
+console.log(Object.getPrototypeOf(c1) === Circle.prototype);//true
+console.log(c1.getCircumReference())
